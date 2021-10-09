9c3919496a0f9640056153eeb293e3eee4618142
JiHun Hong <hongji3354@gmail.com>
2021-10-10 03:12:59 +0900

    feat : 자동차 이름 입력시 console에 띄울 메시지 추가

--- src/test/java/racinggame/view/CarNameConsole.java
+++ src/test/java/racinggame/view/CarNameConsole.java
@@ -18,6 +18,7 @@ public class CarNameConsole {
     }
 
     public static CarNames read() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         final String racingCarName = Console.readLine();
         final List<String> racingCarNames = Arrays.asList(racingCarName.split(RACING_CAR_NAME_DELIMITER));
         validate(racingCarNames);
